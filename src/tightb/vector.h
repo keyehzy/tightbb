73f141e66a7549a2618178d6ed51c227b8377bfe
keyehzy <msamuel@aluno.puc-rio.br>
2022-05-27 10:19:37 -0300

    First commit

---
+++ src/tightb/vector.h
@@ -0,0 +1,56 @@
+// tightb - calculate electronic properties of solids
+// Copyright (C) 2022  Matheus de Sousa (keyehzy)
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+#ifndef TIGHTB_VECTOR_H
+#define TIGHTB_VECTOR_H
+
+#include <array>
+
+template<int S>
+class Vec {
+public:
+    Vec(const std::initializer_list<double> &list) : data_(list) {};
+
+    explicit Vec(int i) : data_(i) {};
+
+    double &operator[](int i);
+
+    Vec<S> operator+(Vec<S> v);
+
+    [[nodiscard]] std::vector<double> data() const { return this->data_; }
+
+private:
+    std::vector<double> data_;
+};
+
+template<int S>
+double &Vec<S>::operator[](int i) {
+  assert(i >= 0 && i < S);
+  return this->data_[i];
+}
+
+template<int S>
+Vec<S> Vec<S>::operator+(Vec<S> v) {
+  Vec<S> new_v(S);
+
+  for (int i = 0; i < S; i++) {
+    new_v[i] = this->data_[i] + v[i];
+  }
+
+  return new_v;
+}
+
+#endif //TIGHTB_VECTOR_H
---
+++ test/vector.cpp
@@ -0,0 +1,32 @@
+// tightb - calculate electronic properties of solids
+// Copyright (C) 2022  Matheus de Sousa (keyehzy)
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+#include <tightb/vector.h>
+
+using ::testing::ElementsAre;
+
+TEST(test_vector, declaration) {
+  Vec<2> v = {1.0, 2.0};
+}
+
+TEST(test_vector, add_two_vectors) {
+  Vec<2> v1{1.0, 2.0};
+  Vec<2> v2{2.0, 3.0};
+  Vec<2> sum = v1 + v2;
+  EXPECT_THAT(sum.data(), ElementsAre(3.0, 5.0));
+}
